13a40e49dd94361196216294e8458566eba6278a
Rpenubarthi <penubarthi.r@northeastern.edu>
2025-10-28 14:35:27 -0400

    Works

--- app/(Kambaz)/Database/page.tsx
+++
@@ -1,7 +0,0 @@
-import courses from "./courses.json";
-import modules from "./modules.json";
-import assignments from "./Assignments.json";
-
-export default {
-    courses, modules, assignments,
-};
