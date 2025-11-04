8ead544d5d9f5df70e5ad8dfc36b61b4e650cc45
Rpenubarthi <penubarthi.r@northeastern.edu>
2025-11-03 23:09:50 -0500

    Made branch A4

---
+++ app/Labs/Lab4/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+export default function Lab4() {
+    return (
+        <div id="wd-lab4">
+            <h2>Lab 4</h2>
+            <h3>Ruthvik Penubarthi Section 01</h3>
+            <a href="https://github.com/Rpenubarthi/WebDevA1">Link to Repository</a>
+        </div>);
+}
--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -17,6 +17,10 @@ export default function Labs() {
                         Lab 3: JavaScript Fundamentals </Link>
                 </li>
                 <li>
+                    <Link href="/Labs/Lab4" id="wd-lab4-link">
+                        Lab 4: React State Applications </Link>
+                </li>
+                <li>
                     <Link href="/" id="wd-lab3-link">
                         Kambaz </Link> </li>
 
