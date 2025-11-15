5068c5b3c259aea327c3f6db825436c0d027feef
Rpenubarthi <penubarthi.r@northeastern.edu>
2025-11-15 02:54:33 -0500

    Minor fixes

--- app/(Kambaz)/Database/index.ts
+++ app/(Kambaz)/Database/index.ts
@@ -1,6 +1,6 @@
 import courses from "./courses.json";
 import modules from "./modules.json";
-import assignments from "./assignments.json";
+import assignments from "./Assignments.json";
 
 export {
     courses, modules, assignments,
